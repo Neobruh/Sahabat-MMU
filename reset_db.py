b413b0e5d806890bbd2bca83a5c2d6311fd40152
Neo <matthiasneo2@gmail.com>
2026-05-04 16:02:08 +0800

    fixed stuff

--- reset_db.py
+++ reset_db.py
@@ -1,6 +1,8 @@
-from db import app, db
+from app2 import app
+from db import db
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
 
-print("Database created!")
+print("Database reset complete!")
